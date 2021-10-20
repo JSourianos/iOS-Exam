a5d85b8ac70dda0aa94a472c4e000a7ed85b5ad3
Thomas Sourianos <thomasourianos@live.no>
2021-10-20 16:20:15 +0200

    Initial Commit

---
+++ iOS Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Exam
+//
+//  Created by Thomas Sourianos on 20/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
